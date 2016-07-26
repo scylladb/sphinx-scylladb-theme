4837a6ea568a090f4237d21f54d5d2f339f5845b
Don Marti <dmarti@scylladb.com>
2016-07-26 15:03:11 -0700

    Update project name in config
    Signed-off-by: Don Marti <dmarti@scylladb.com>

--- conf.py
+++ conf.py
@@ -52,7 +52,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Scylla Documentation'
+project = u'Scylla'
 copyright = u'2016, Scylla Project Contributors'
 author = u'Scylla Project Contributors'
 
