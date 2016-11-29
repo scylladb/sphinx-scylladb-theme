eee46a8cf81ea2c049b5a4bdbf3258c87c840116
ray <hanlray@gmail.com>
2016-11-29 09:29:03 +0800

    ubuntu docs converted

--- conf.py
+++ conf.py
@@ -33,7 +33,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
     'sphinx.ext.githubpages',
-    'sphinx.ext.autosectionlabel'
+#    'sphinx.ext.autosectionlabel'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
