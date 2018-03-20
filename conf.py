81ad0ec48c78ce09b25553c1d930c0750a8675a7
Tzach Livyatan <tzach.livyatan@gmail.com>
2018-03-20 10:03:45 +0200

    do not renders files under rst_include dir (#831)

--- conf.py
+++ conf.py
@@ -100,7 +100,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'lib', 'lib64','**/_common/*', 'README.md', '.git', '_utils', '_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'lib', 'lib64','**/_common/*', 'README.md', '.git', '_utils', '_templates', 'rst_include']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
