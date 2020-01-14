06c0b08bb432ef6490422508df133a787257b257
Tzach Livyatan <tzach@scylladb.com>
2020-01-14 10:13:28 +0200

    add an empty style option, removing the basic.css file (scylladb/scylla-docs#2351)

--- conf.py
+++ conf.py
@@ -169,6 +169,8 @@ todo_include_todos = True
 #
 html_theme = 'basic'
 
+html_style = ''
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
