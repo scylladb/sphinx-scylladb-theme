e72d863767301f39b39d35f2b4132ec2792342f7
dgarcia360 <dgarcia360@outlook.com>
2020-09-21 15:50:57 +0200

    Added redirects multiversion support

--- sphinx_scylladb_theme/extensions/redirects.py
+++ sphinx_scylladb_theme/extensions/redirects.py
@@ -7,20 +7,25 @@ def write_html_redirect(redirect_to):
     return html
 
 def create_redirects(app, docname):
-    if not app.config.redirects_file:
+    redirects_file = app.config.redirects_file
+    if not redirects_file:
         return
-    if not os.path.exists(app.config.redirects_file):
+    if os.path.exists('docs'):
+        redirects_file = 'docs/' + redirects_file
+    if not os.path.exists(redirects_file):
         return
-    if not app.builder.name == 'dirhtml':
-        return
-    with open(app.config.redirects_file, 'r') as yaml_file:
+    with open(redirects_file, 'r') as yaml_file:
         full_load = yaml.full_load(yaml_file)
         if full_load:
             for from_path, redirect_to in full_load.items():
                 target_path = app.outdir + '/' + from_path
+                if os.getenv("SPHINX_MULTIVERSION_NAME") is not None:
+                    redirect_to = '/' + os.environ['SPHINX_MULTIVERSION_NAME'] + redirect_to
                 if not os.path.exists(target_path):
                     os.makedirs(target_path)
                 with open(os.path.join(target_path + '/index.html'), 'w') as t_file:
+                    t_file.write(write_html_redirect(redirect_to))
+                with open(os.path.join(target_path + '.html'), 'w') as t_file:
                     t_file.write(write_html_redirect(redirect_to))
 
 def setup(app):
