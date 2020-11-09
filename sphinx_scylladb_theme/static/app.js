123728f279eabf9b693f9c9c9d9d2e22f215cb13
David Garcia <dgarcia360@outlook.com>
2020-11-09 12:03:39 +0000

    Overwrite internal links classification (#71)
    * Overwrites the links classification
    
    * Clean code

--- sphinx_scylladb_theme/static/app.js
+++ sphinx_scylladb_theme/static/app.js
@@ -10,6 +10,16 @@ $(document).ready(function() {
 
 // Opens external links in a new tab
 $(document).ready(function () {
-  $('a.external').attr('target', '_blank');
+    const isExternal = new RegExp('^(?:[a-z]+:)?//', 'i');
+    $('a.reference').each(function() {
+        $(this).removeClass('internal external');
+
+        if (isExternal.test($(this).attr('href'))) {
+            $(this).addClass('external');
+            $(this).attr('target', '_blank')
+        } else {
+            $(this).addClass('internal');
+        }
+    });  
 });
 
