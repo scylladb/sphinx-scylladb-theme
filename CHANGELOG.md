4b14e12c109c42d0bc3c414255eef0fe84f3ab1f
dgarcia360 <dgarcia360@outlook.com>
2020-11-17 14:00:05 +0100

    Make non latest page clearer

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.1.15] - 17 Nov 2020
+
+### Added
+
+- Warning when not reading the latest version.
+
 ## [0.1.14] - 28 Oct 2020
 
 ### Changed
--- sphinx_scylladb_theme/__init__.py
+++ sphinx_scylladb_theme/__init__.py
@@ -1,11 +1,13 @@
-from os import path
+from os import path, getenv
 import sphinx_copybutton
 from sphinx_tabs import tabs
 from .extensions import panel_box, topic_box, redirects, not_found, substitutions
 
 def setup(app):
-    """Setup theme and custom extensions."""
+    """Setup theme"""
     app.add_html_theme('sphinx_scylladb_theme', path.abspath(path.dirname(__file__)))
+    app.config.smv_latest_version = getenv('LATEST_VERSION', default='master')
+    """Setup custom extensions"""
     panel_box.setup(app)
     topic_box.setup(app)
     substitutions.setup(app)
