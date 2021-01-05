2c7c854fb9b8ed3624a315e6e0fa1b9bdc4481de
dgarcia360 <dgarcia360@outlook.com>
2021-01-05 12:06:33 +0100

    Added version in footer
    Added version in footer

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.1.19] - Next
+
+### Added
+
+- Sphinx and theme version in the footer.
+
 ## [0.1.18] - 31 Dec 2020
 
 ### Added
--- sphinx_scylladb_theme/__init__.py
+++ sphinx_scylladb_theme/__init__.py
@@ -1,11 +1,16 @@
 from os import path, getenv
 import sphinx_copybutton
 from sphinx_tabs import tabs
-from .extensions import panel_box, topic_box, redirects, not_found, gh_pages
+from sphinx_scylladb_theme.extensions import panel_box, topic_box, redirects, not_found, gh_pages
+from sphinx_scylladb_theme._version import get_version
+
+def update_context(app, pagename, templatename, context, doctree):
+    context["scylladb_theme_version"] = get_version()
 
 def setup(app):
     """Setup theme"""
     app.add_html_theme('sphinx_scylladb_theme', path.abspath(path.dirname(__file__)))
+    app.connect("html-page-context", update_context)
     app.config.smv_latest_version = getenv('LATEST_VERSION', default='master')
     """Setup custom extensions"""
     panel_box.setup(app)
@@ -17,3 +22,4 @@ def setup(app):
     sphinx_copybutton.setup(app)
     tabs.setup(app)
     gh_pages.setup(app)
+    return {"version": get_version(), "parallel_read_safe": True}
