78d20091d716b037ff3189d82e1924d757b6380f
dgarcia360 <dgarcia360@outlook.com>
2021-06-09 09:52:57 +0100

    Add new dropdown

--- sphinx_scylladb_theme/__init__.py
+++ sphinx_scylladb_theme/__init__.py
@@ -36,11 +36,32 @@ def update_context(app, pagename, templatename, context, doctree):
     context["navigation_tree"] = compute_toc_tree(context)
 
 
-def update_config(app, config):
+def override_smv_latest_version(config):
     default = "master"
     if hasattr(config, "smv_latest_version") and config.smv_latest_version:
         default = config.smv_latest_version
     config.smv_latest_version = getenv("LATEST_VERSION", default=default)
+    return config.smv_latest_version
+
+
+def override_rst_prolog(config):
+    substitutions = """
+    .. |v| raw:: html
+
+        <i class="inline-icon fa fa-check" aria-hidden="true"></i>
+
+    .. |x| raw:: html
+
+        <i class="inline-icon fa fa-times" aria-hidden="true"></i>
+    """
+    rst_prolog = config.rst_prolog or ""
+    config.rst_prolog = substitutions + rst_prolog
+    return config.rst_prolog
+
+
+def update_config(app, config):
+    override_smv_latest_version(config)
+    override_rst_prolog(config)
 
 
 def setup(app):
