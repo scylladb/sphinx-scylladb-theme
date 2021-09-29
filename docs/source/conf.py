d2a771b3120a73553b5d560e088e8eb7c6fcf659
David Garcia <dgarcia360@outlook.com>
2021-09-29 15:48:45 +0100

    Add breadcrumb highlight (#214)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,7 @@ autosectionlabel_prefix_document = True
 master_doc = "index"
 
 # General information about the project.
-project = "Scylla Documentation Theme and Extensions"
+project = "Scylla Sphinx Theme"
 copyright = str(date.today().year) + ", ScyllaDB. All rights reserved."
 author = u"Scylla Project Contributors"
 
