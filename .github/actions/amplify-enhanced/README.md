f7fe7ee59d0fa785073260109208163986d334a8
David Garcia <hi@davidgarcia.dev>
2022-09-15 10:10:01 +0100

    Add amplify enhanced action
    Add amplify enhanced action

---
+++ .github/actions/amplify-enhanced/README.md
@@ -0,0 +1,62 @@
+# Amplify enhanced action
+
+GitHub Action extension that powers up AWS Amplify with preview links for every change edited.
+
+## Motivation
+
+Amplify publishes a message for every pull request with a link to the preview site:
+
+![image](img/amplify-preview.png)
+
+This allows reviewers to preview how the changes will look like in the documentation site. However, they have to surf the site to find the specific documentation page(s) that have received an update.
+
+When using the Amplify enhanced action, the message generates a preview URL for every file changed:
+
+![image](img/amplify-enhanced-preview.png)
+
+## Installation
+
+Sample workflow `.github/workflows/docs-amplify-enhanced.yml`
+
+```
+name: "Docs / Amplify enhanced"
+
+on: issue_comment
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    if: ${{ github.event.issue.pull_request }}
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v3
+        with:
+          fetch-depth: 0
+      - name: Amplify enhanced
+        env:
+          TOKEN: ${{ secrets.GITHUB_TOKEN }}
+        uses: scylladb/sphinx-scylladb-theme/.github/actions/amplify-enhanced@master
+```
+
+## How it works
+
+A workflow triggers the Amplify enhanced action when AWS publishes the preview message on the pull request. Then, Amplify enhanced action:
+
+1. Reads the AWS Amplify message and parses the preview URL.
+2. Detects which Markdown and restructuredText files within the docs folder received an update.
+3. Computes the preview URLs for each documentation file.
+4. Edits the original AWS Amplify message with the preview URLs for each documentation page.
+
+**Exception**
+
+Amplify enhanced action deletes the Amplify preview message if no documentation files are changed.
+
+## Sponsors
+
+This project is sponsored by Scylla, The Real-Time Big Data Database.
+
+## License
+
+Copyright (c) 2022 - present David Garcia (@dgarcia360).
+
+Licensed under the MIT License.
---
+++ .github/actions/amplify-enhanced/index.js
@@ -0,0 +1,88 @@
+/**
+ * Copyright (c) 2022 - present David Garcia (@dgarcia360)
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+const core = require("@actions/core");
+const github = require("@actions/github");
+const { Octokit } = require("octokit");
+
+async function main() {
+  // Authenticate
+  const octokit = new Octokit({
+    auth: process.env.TOKEN,
+  });
+
+  // Search the Amplify URL
+  const url = bodyComment.split("Access this pull request here: ")[1];
+
+  // Get the pull request files modified
+  const request = {
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    pull_number: github.context.issue.number,
+  };
+
+  let changedFiles;
+  try {
+    const result = await octokit.request(
+      `GET /repos/${request.owner}/${request.repo}/pulls/${github.context.issue.number}/files`,
+      request
+    );
+    changedFiles = result.data;
+  } catch (err) {
+    core.setFailed(`Request failed with error ${err}`);
+  }
+
+  // For each changed file, get the files under **/docs, .md or .rst files
+  const acceptedFilesStatus = ["created", "modified"];
+  let docFiles = [];
+  changedFiles.forEach((file) => {
+    if (
+      acceptedFilesStatus.includes(file.status) &&
+      file.filename.startsWith("docs/") &&
+      (file.filename.endsWith(".rst") || file.filename.endsWith(".md"))
+    ) {
+      const baseFilename = file.filename
+        .replace("docs/", "")
+        .split(".")
+        .slice(0, -1)
+        .join(".");
+      docFiles.push(`- ${file.filename} ${url}/${baseFilename}\n`);
+    }
+  });
+
+  if (docFiles.length == 0) {
+    // Delete the comment
+    try {
+      await octokit.rest.issues.deleteComment({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        comment_id: github.context.payload.comment.id,
+      });
+    } catch (err) {
+      core.setFailed(`Delete comment failed with error ${err}`);
+    }
+  } else {
+    // Update the original comment of the pull request to add the list of modified files
+    try {
+      await octokit.rest.issues.updateComment({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        comment_id: github.context.payload.comment.id,
+        body: `${bodyComment}\n\nFiles changed:\n${docFiles}`,
+      });
+    } catch (err) {
+      core.setFailed(`Comment failed with error ${err}`);
+    }
+  }
+}
+
+const bodyComment = github.context.payload.comment.body;
+const amplifyComment = `${bodyComment}`.includes(
+  "This pull request is automatically being deployed by Amplify Hosting"
+);
+if (github.context.payload.issue.pull_request && amplifyComment) {
+  main();
+}
