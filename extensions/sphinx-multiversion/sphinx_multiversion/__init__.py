ae7c5b5e32c8326aeaeb524d1acd2ef136d3b2e9
David Garcia <hi@davidgarcia.dev>
2022-09-16 10:23:54 +0100

    Add multiversion extension (#555)

---
+++ extensions/sphinx-multiversion/sphinx_multiversion/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from .main import main
+from .sphinx import setup
+
+__version__ = "0.2.4"
+
+__all__ = [
+    "setup",
+    "main",
+]
---
+++ extensions/sphinx-multiversion/sphinx_multiversion/__main__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+
+from .main import main
+
+sys.exit(main())
