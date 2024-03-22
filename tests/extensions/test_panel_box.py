f88904e789ad2a47beaa20855a0f828b7272a2e6
David Garcia <hi@davidgarcia.dev>
2024-03-22 10:00:49 +0000

    fix: tests (#1052)

--- tests/extensions/test_panel_box.py
+++ tests/extensions/test_panel_box.py
@@ -1,3 +1,4 @@
+from unittest.mock import Mock
 import pytest
 from bs4 import BeautifulSoup as bs
 
@@ -16,10 +17,14 @@ test_data = [
     ],
 ]
 
+mock_state_machine = Mock()
+mock_state_machine.reporter = Mock()
+mock_state = Mock()
+
 
 @pytest.mark.parametrize("arguments, options, content, expected", test_data)
 def test(arguments, options, content, expected):
-    directive = PanelBox("component", arguments, options, content, 0, 0, "", None, None)
+    directive = PanelBox("component", arguments, options, content, 0, 0, "", mock_state_machine, mock_state)
     result = directive.run()
     assert (
         bs(result[0].astext(), "html.parser").prettify()
