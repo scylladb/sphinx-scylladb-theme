1c80df1728883dc1696a0b12abef9332449966b0
David Garcia <hi@davidgarcia.dev>
2024-05-07 07:51:44 +0100

    fix highlighting right sidebar (#1092)

--- src/js/secondary-sidebar.js
+++ src/js/secondary-sidebar.js
@@ -2,24 +2,23 @@ export class SecondarySidebarHandler {
 
 	onScrollHighlightSecondarySidebar() {
 		const sections = $(".content").find("h2").parent();
+		const offset = 1;
 
 		$(window).scroll(function () {
 			const dynamicPaddingTop = parseInt($('html').css('scroll-padding-top'));
 			const currentScroll = $(this).scrollTop() + dynamicPaddingTop;
+	
 			sections.each(function () {
 				const sectionPosition = $(this).offset().top;
-				if (sectionPosition <= currentScroll) {
+				if (sectionPosition - offset <= currentScroll) {
 					const id = $(this).attr("id");
-
 					$(".secondary-side-nav a").removeClass("current");
 					$('.secondary-side-nav a[href="#' + id + '"]').addClass("current");
 				}
 			});
 		});
-
 	}
-
-	init() {
+		init() {
 		this.onScrollHighlightSecondarySidebar();
 	}
 }
