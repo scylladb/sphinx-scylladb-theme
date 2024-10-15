89e0caacf3694f308c8fa0259207d379c0d220fd
David Garcia <hi@davidgarcia.dev>
2024-10-15 07:42:40 +0100

    chore: replace localstorage with cache

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -1,28 +1,47 @@
+import Cookies from 'js-cookie';
+
 export class DarkTheme {
 
-
-    init() {
-        let state = localStorage.getItem('dark-theme') === 'true';
-        let rootDocumentEl = $("html");
-        let darkThemeTogglerEl = $(".dark-theme-toggler");
-
-
-        const toggler = (state) => {
-            if (state) {
-                rootDocumentEl.addClass('dark')
-            } else {
-                rootDocumentEl.removeClass('dark')
-            }
-        }
-
-        toggler(state);
-
-
-        darkThemeTogglerEl.on('click', () => {
-            state = !state;
-            localStorage.setItem('dark-theme', state);
-            toggler(state)
-        });
+    constructor() {
+        this.cacheKey = "scylladb-docs-dark-theme";
+        this.darkThemeClass = "dark";
+        this.cookieOptions = {
+            expires: 365,
+            path: '/'
+        };
+        this.rootDocumentEl = $("html");
+        this.darkThemeTogglerEl = $(".dark-theme-toggler");
     }
 
+    getInitialState() {
+        let state = Cookies.get(this.cacheKey);
+
+        if (state === undefined) {
+            return window.matchMedia('(prefers-color-scheme: dark)').matches;
+        }
+        return (state === 'true');
+    }
+
+    applyTheme(state) {
+        if (state) {
+            this.rootDocumentEl.addClass(this.darkThemeClass);
+        } else {
+            this.rootDocumentEl.removeClass(this.darkThemeClass);
+        }
+    }
+
+    saveThemeState(state) {
+        Cookies.set(this.cacheKey, state, this.cookieOptions);
+    }
+
+    init() {
+        let state = this.getInitialState();
+        this.applyTheme(state);
+
+        this.darkThemeTogglerEl.on('click', () => {
+            state = !state;
+            this.saveThemeState(state);
+            this.applyTheme(state);
+        });
+    }
 }
