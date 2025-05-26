b7e36a7ea137a64fce4ac0c05df9cd3adc8eef80
David Garcia <hi@davidgarcia.dev>
2025-05-26 21:35:29 +0200

    fix: sidebar sroll (#1428)

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -5,63 +5,27 @@ export class SidebarHandler {
   }
   
   initScrollEvent() {
-    $('#side-nav').scroll(() => {
-      const scrollPosition = $('#side-nav').scrollTop();
+    const sideNavContent = $('#side-nav .side-nav-content');
+    sideNavContent.on('scroll', () => {
+      const scrollPosition = sideNavContent.scrollTop();
       localStorage.setItem(this.localStorageKey, scrollPosition);
+      console.log('scrollPosition', scrollPosition);
     });
   }
 
   onLoadRestoreScrollPosition() {
+    const sideNavContent = $('#side-nav .side-nav-content');
     const savedPosition = localStorage.getItem(this.localStorageKey);
+    console.log('savedPosition', savedPosition);
     if (savedPosition) {
-      $('#side-nav').scrollTop(savedPosition);
+      sideNavContent.scrollTop(savedPosition);
     }
-    $('.side-nav-content').css('visibility', 'visible');
+    // $('.side-nav-content').css('visibility', 'visible');
   }
 
-  calculateScrollOffset(selectors) {
-    let totalHeight = 0;
-  
-    selectors.forEach(selector => {
-      const element = document.querySelector(selector);
-      if (element) {
-        totalHeight += element.offsetHeight;
-      }
-    });
-    return totalHeight;
-  }
-  
-  adjustSidebarHeightFooter() {
-    const sidebar = document.querySelector('.side-nav');
-    const secondarySidebar = document.querySelector('.secondary-side-nav');
-    if ($(window).innerWidth() >= 1024) {
-      const windowHeight =  $(window).innerHeight();
-      const headerOffset = this.calculateScrollOffset(['.header', '.promo-banner']);
-      const footerTop = document.querySelector('.footer').getBoundingClientRect().top;
-      const visibleFooterHeight = footerTop > windowHeight ? 0 : windowHeight - footerTop;
-      const offset = headerOffset + visibleFooterHeight;
-      if (visibleFooterHeight > 0 && footerTop < windowHeight) {
-        sidebar.style.height = `calc(100vh - ${offset}px)`;
-        secondarySidebar.style.height = `calc(100vh - ${offset}px)`;
-      } else {
-        sidebar.style.height = '100vh';
-        secondarySidebar.style.height = '100vh';
-      }
-    } else {
-      sidebar.style.removeProperty('height');
-      secondarySidebar.style.removeProperty('height');
-    }
-  }
-  
+    
   init() {
-    // this.initScrollEvent();
-    // this.onLoadRestoreScrollPosition();
-
-    // const adjustForDesktopView = () => {
-    //   this.adjustSidebarHeightFooter();
-    // };
-
-    // $(window).scroll(adjustForDesktopView);
-    // $(window).resize(adjustForDesktopView);
+    this.initScrollEvent();
+    this.onLoadRestoreScrollPosition();
   }
 }
